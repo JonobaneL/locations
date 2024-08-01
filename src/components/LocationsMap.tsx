36b6cbb6847d74f3de58819326295026d77755b9
jonobane <jonobane@gmail.com>
2024-08-01 11:28:41 +0300

    fix style load error

--- src/components/LocationsMap.tsx
+++ src/components/LocationsMap.tsx
@@ -1,10 +1,4 @@
-import {
-  Map,
-  MapMouseEvent,
-  MapRef,
-  NavigationControl,
-  ViewStateChangeEvent,
-} from "react-map-gl";
+import { Map, MapMouseEvent, MapRef, NavigationControl } from "react-map-gl";
 import MapCluster from "./MapCluster";
 import MapPoint from "./MapPoint";
 import MapNav from "./MapNav";
@@ -16,12 +10,11 @@ type MapProps = {
   points: PointParams[];
   setPoints: React.Dispatch<React.SetStateAction<PointParams[]>>;
 };
-
 const LocationsMap = ({ points, setPoints }: MapProps) => {
   const initialPosition = { lat: 50.390438, lng: 30.451204 };
   const mapRef = useRef<MapRef>(null);
   const [visibleMarks, setVisible] = useState(true);
-
+  const [styleLoaded, setStyleLoaded] = useState(false);
   const clickHandler = (e: MapMouseEvent) => {
     const map = mapRef?.current?.getMap();
     const zoom = map?.getZoom() || 0;
@@ -62,35 +55,48 @@ const LocationsMap = ({ points, setPoints }: MapProps) => {
       setVisible(true);
     }
   };
+
   return (
     <div className="w-full h-fit p-10 relative">
-      <Map
-        ref={mapRef}
-        mapboxAccessToken={import.meta.env.VITE_MAPBOX_API_KEY}
-        initialViewState={{
-          longitude: initialPosition.lng,
-          latitude: initialPosition.lat,
-          zoom: 10,
-        }}
-        onClick={clickHandler}
-        style={{ width: "100%", height: "40rem" }}
-        onMove={() => onMoveHandler()}
-        mapStyle="mapbox://styles/jonobane/clz9qbwvp004201qw3pdr2q9n"
-      >
-        {visibleMarks &&
-          points.map((item, index) => (
-            <MapPoint
-              key={index}
-              index={index + 1}
-              point={item}
-              points={points}
-              onChange={setPoints}
-            />
-          ))}
+      {true && (
+        <Map
+          ref={mapRef}
+          mapboxAccessToken={import.meta.env.VITE_MAPBOX_API_KEY}
+          initialViewState={{
+            longitude: initialPosition.lng,
+            latitude: initialPosition.lat,
+            zoom: 10,
+          }}
+          styleDiffing={true}
+          onLoad={() => setStyleLoaded(true)}
+          onClick={clickHandler}
+          style={{ width: "100%", height: "40rem" }}
+          onMove={() => onMoveHandler()}
+          mapStyle="mapbox://styles/jonobane/clz9qbwvp004201qw3pdr2q9n"
+        >
+          {styleLoaded ? (
+            <>
+              {visibleMarks &&
+                points.map((item, index) => (
+                  <MapPoint
+                    key={index}
+                    index={index + 1}
+                    point={item}
+                    points={points}
+                    onChange={setPoints}
+                  />
+                ))}
+              <MapCluster marks={points} />
+              <NavigationControl position="top-right" />
+            </>
+          ) : (
+            <>
+              <h1 className="text-center font-bold text-2xl"> Loading...</h1>
+            </>
+          )}
+        </Map>
+      )}
 
-        <MapCluster marks={points} />
-        <NavigationControl position="top-right" />
-      </Map>
       {points.length > 0 && (
         <MapNav data={points} callback={() => setPoints([])} />
       )}
